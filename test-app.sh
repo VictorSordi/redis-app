8931cd11f9b705d1718beaad9c473b96d1d698b8
VictorSordi <victorsordi200301@outlook.com>
2024-12-04 18:38:19 -0300

    Alterado test-app.sh

---
+++ test-app.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+RESULT="`wget -qO- http://localhost:8090`"
+wget -q localhost:8090
+if [$? -eq 0]
+then
+    echo 'Ok - service ON'
+elif [[ $RESULT == *"Number"* ]]
+then
+    echo 'OK - number of visits'
+    echo $RESULT
+else
+    echo 'OFF - number of visits'
+    exit 1
+fi

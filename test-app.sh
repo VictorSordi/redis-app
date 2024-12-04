df0500d57ce47ed4e8db3efe061bbd4f1bfb466b
VictorSordi <victorsordi200301@outlook.com>
2024-12-04 18:55:58 -0300

    Alterado test-app.sh

--- test-app.sh
+++ test-app.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 RESULT="`wget -qO- http://localhost:8090`"
+echo "RESULT: $RESULT"  # Debugging line to print the result
 wget -q localhost:8090
 if [ $? -eq 0 ]
 then
     echo 'Ok - service ON'
-elif [ "$RESULT" = *"Number"* ]
+elif [[ "$RESULT" == *"Number"* ]]
 then
     echo 'OK - number of visits'
     echo "$RESULT"
 else
     echo 'OFF - number of visits'
+    echo "DEBUG: $RESULT"  # Debugging line
     exit 1
 fi
